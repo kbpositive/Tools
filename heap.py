1fcb8a0b93959dd169370d8dad2b1063c6525c01
Kadeem <kadbaldwin@gmail.com>
2020-08-20 21:39:56 -0500

    Trimmed heap file to 'delete' and 'insert' functions.

--- heap.py
+++ heap.py
@@ -1,89 +1,37 @@
-import random
-
-# create max heap
-rando = random.sample(range(200),11)
-max_heap = []
-for num in rando:
-    max_heap.append(num)
-
-    element = len(max_heap) - 1
-    while element > 0 and max_heap[(element - 1) // 2] < max_heap[element]:
-        temp = max_heap[element]
-        max_heap[element] = max_heap[(element - 1) // 2]
-        max_heap[(element - 1) // 2] = temp
-        element = (element - 1) // 2
-
-print(max_heap)
-
-# add element
-for i in range(10):
-    max_heap.append(random.randrange(200))
-
-    element = len(max_heap) - 1
-    while element > 0 and max_heap[(element - 1) // 2] < max_heap[element]:
-        temp = max_heap[element]
-        max_heap[element] = max_heap[(element - 1) // 2]
-        max_heap[(element - 1) // 2] = temp
-        element = (element - 1) // 2
-
-print(max_heap)
-
-# delete element
-for i in range(10):
-    item = max_heap[0]
-
-    max_heap[0] = max_heap[-1]
-    max_heap.pop()
+def delete(type, heap: list):
+    item = heap[0]
+    heap[0] = heap[-1]
+    heap.pop()
     element = 0
-    while (2*element + 2) < len(max_heap) and max([max_heap[2*element + 1], max_heap[2*element + 2]]) > max_heap[element]:
-        child = 2 if max_heap[2*element + 2] > max_heap[2*element + 1] else 1
-        temp = max_heap[2*element + child]
-        max_heap[2*element + child] = max_heap[element]
-        max_heap[element] = temp
-        element = 2*element + child
+    if type == 'max':
+        direction = lambda: max([heap[2 * element + 1], heap[2 * element + 2]]) > heap[element]
+        right_child = lambda: heap[2 * element + 2] > heap[2 * element + 1]
+    elif type == 'min':
+        direction = lambda: min([heap[2 * element + 1], heap[2 * element + 2]]) < heap[element]
+        right_child = lambda: heap[2 * element + 2] < heap[2 * element + 1]
+    else:
+        raise KeyError('\'type\' must be either \'min\' or \'max\'.')
 
-print(max_heap)
+    while (2 * element + 2) < len(heap) and direction():
+        child = 2 if right_child() else 1
+        temp = heap[2 * element + child]
+        heap[2 * element + child] = heap[element]
+        heap[element] = temp
+        element = 2 * element + child
+    return item
 
-# create min heap
-rando = random.sample(range(200),11)
-min_heap = []
-for num in rando:
-    min_heap.append(num)
+def insert(type, item, heap: list) -> object:
+    heap.extend([item])
+    element = len(heap) - 1
+    if type == 'max':
+        direction = lambda: heap[(element - 1) // 2] < heap[element]
+    elif type == 'min':
+        direction = lambda: heap[(element - 1) // 2] > heap[element]
+    else:
+        raise KeyError('\'type\' must be either \'min\' or \'max\'.')
 
-    element = len(min_heap) - 1
-    while element > 0 and min_heap[(element - 1) // 2] > min_heap[element]:
-        temp = min_heap[element]
-        min_heap[element] = min_heap[(element - 1) // 2]
-        min_heap[(element - 1) // 2] = temp
+    while element > 0 and direction():
+        temp = heap[element]
+        heap[element] = heap[(element - 1) // 2]
+        heap[(element - 1) // 2] = temp
         element = (element - 1) // 2
-
-print(min_heap)
-
-# add element
-for i in range(10):
-    min_heap.append(random.randrange(200))
-
-    element = len(min_heap) - 1
-    while element > 0 and min_heap[(element - 1) // 2] > min_heap[element]:
-        temp = min_heap[element]
-        min_heap[element] = min_heap[(element - 1) // 2]
-        min_heap[(element - 1) // 2] = temp
-        element = (element - 1) // 2
-
-print(min_heap)
-
-# delete element
-for i in range(10):
-    item = min_heap[0]
-
-    min_heap[0] = min_heap[-1]
-    min_heap.pop()
-    element = 0
-    while (2*element + 2) < len(min_heap) and min([min_heap[2*element + 1], min_heap[2*element + 2]]) < min_heap[element]:
-        child = 2 if min_heap[2*element + 2] < min_heap[2*element + 1] else 1
-        temp = min_heap[2*element + child]
-        min_heap[2*element + child] = min_heap[element]
-        min_heap[element] = temp
-        element = 2*element + child
-
-print(min_heap)
