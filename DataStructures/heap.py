4363df4efa31b58574ec2c2f7b6249b00b4a11c7
Kadeem <kadbaldwin@gmail.com>
2020-08-23 15:07:42 -0500

    Fixed heap delete function.

--- DataStructures/heap.py
+++ DataStructures/heap.py
@@ -4,21 +4,28 @@ def delete(type, heap: list):
     heap.pop()
     element = 0
     if type == 'max':
-        direction = lambda: max([heap[2 * element + 1], heap[2 * element + 2]]) > heap[element]
-        right_child = lambda: heap[2 * element + 2] > heap[2 * element + 1]
+        direction = lambda x : max([heap[2 * element + i] for i in range(1, x+1)]) > heap[element]
+        right_child = lambda: heap[2 * element + 1] > heap[2 * element + 2]
     elif type == 'min':
-        direction = lambda: min([heap[2 * element + 1], heap[2 * element + 2]]) < heap[element]
-        right_child = lambda: heap[2 * element + 2] < heap[2 * element + 1]
+        direction = lambda x : min([heap[2 * element + i] for i in range(1, x+1)]) < heap[element]
+        right_child = lambda: heap[2 * element + 1] < heap[2 * element + 2]
     else:
         raise KeyError('\'type\' must be either \'min\' or \'max\'.')
 
-    while (2 * element + 2) < len(heap) and direction():
-        child = 2 if right_child() else 1
+    while (2 * element + 1) <= len(heap) - 1:
+        if (2 * element + 2) <= (len(heap) - 1) and direction(2):
+            child = 1 if right_child() else 2
+        elif direction(1):
+            child = 1
+        else:
+            break
+
         temp = heap[2 * element + child]
         heap[2 * element + child] = heap[element]
         heap[element] = temp
         element = 2 * element + child
     return item
+
 
 def insert(type, item, heap: list) -> object:
     heap.extend([item])
@@ -35,3 +42,4 @@ def insert(type, item, heap: list) -> object:
         heap[element] = heap[(element - 1) // 2]
         heap[(element - 1) // 2] = temp
         element = (element - 1) // 2
+
