432eda92a8a29dd55e1ed4b1e1cd9b5e8ec9392d
Kadeem <kadbaldwin@gmail.com>
2020-08-23 22:51:21 -0500

    Finished quicksort algorithm.

--- Algorithms/quicksort.py
+++ Algorithms/quicksort.py
@@ -1,1 +1,25 @@
-import random
+def partition(array: list, pivot: int, left: int, right: int) -> int:
+    while left <= right:
+        while array[left] < pivot:
+            left += 1
+        while array[right] > pivot:
+            right -= 1
+        if left <= right:
+            temp = array[left]
+            array[left] = array[right]
+            array[right] = temp
+            left += 1
+            right -= 1
+    return left
+
+def quicksort(array: list, left: int, right: int) -> list:
+    if left >= right:
+        return array
+    pivot = array[(left + right) // 2]
+    index = partition(array, pivot, left, right)
+    quicksort(array, left, index - 1)
+    quicksort(array, index, right)
+
+def sort(array: list) -> list:
+    quicksort(array, 0, len(array) - 1)
+    return array
