ab4d9db20fc5cec9eafce0c49213d4c1f38fdb62
Kadeem <kadbaldwin@gmail.com>
2020-08-29 18:27:35 -0500

    Cleanup.

---
+++ Algorithms/depth_first_search.py
@@ -0,0 +1,17 @@
+# for dictionary graphs
+def dfs_dict(graph, function, visited=None):
+    if visited is None:
+        visited = set()
+    function(graph)
+    for node in graph:
+        if node not in visited:
+            visited.add(node)
+            dfs_dict(node, function, visited)
+
+
+# for DataStructures.Types.tree.TreeNode types
+def dfs_tree(tree, function):
+    function(tree)
+    for child in tree.node:
+        if child:
+            dfs_tree(child, function)
--- DataStructures/heap.py
+++ DataStructures/heap.py
@@ -1,51 +1,62 @@
-def delete(type, heap: list):
-    item = heap[0]
-    heap[0] = heap[-1]
-    heap.pop()
-    element = 0
-    if type == 'max':
-        direction = lambda x : max([heap[2 * element + i] for i in range(1, x+1)]) > heap[element]
-        right_child = lambda: heap[2 * element + 1] > heap[2 * element + 2]
-    elif type == 'min':
-        direction = lambda x : min([heap[2 * element + i] for i in range(1, x+1)]) < heap[element]
-        right_child = lambda: heap[2 * element + 1] < heap[2 * element + 2]
-    else:
-        raise KeyError('\'type\' must be either \'min\' or \'max\'.')
+class Heap:
+    def __init__(self, heap_type='max', vals=None):
+        if vals is None:
+            vals = []
+        self.vals = []
+        self.type = heap_type
+        self.insert(vals)
 
-    while (2 * element + 1) <= len(heap) - 1:
-        if (2 * element + 2) <= (len(heap) - 1) and direction(2):
-            child = 1 if right_child() else 2
-        elif direction(1):
-            child = 1
-        else:
-            break
+    def delete(self):
+        item = self.vals[0]
+        self.vals[0] = self.vals[-1]
+        self.vals.pop()
+        element = 0
 
-        temp = heap[2 * element + child]
-        heap[2 * element + child] = heap[element]
-        heap[element] = temp
-        element = 2 * element + child
-    return item
+        def direction(children: int) -> bool:
+            if self.type == 'max':
+                return max([self.vals[2 * element + i] for i in range(1, children + 1)]) > self.vals[element]
+            if self.type == 'min':
+                return min([self.vals[2 * element + i] for i in range(1, children + 1)]) < self.vals[element]
+            raise KeyError('\'type\' must be either \'min\' or \'max\'.')
 
+        def right_child() -> bool:
+            if self.type == 'max':
+                return self.vals[2 * element + 1] > self.vals[2 * element + 2]
+            if self.type == 'min':
+                return self.vals[2 * element + 1] < self.vals[2 * element + 2]
+            raise KeyError('\'type\' must be either \'min\' or \'max\'.')
 
-def insert(type, item, heap: list):
-    heap.extend([item])
-    element = len(heap) - 1
-    if type == 'max':
-        direction = lambda: heap[(element - 1) // 2] < heap[element]
-    elif type == 'min':
-        direction = lambda: heap[(element - 1) // 2] > heap[element]
-    else:
-        raise KeyError('\'type\' must be either \'min\' or \'max\'.')
+        while (2 * element + 1) <= len(self.vals) - 1:
+            if (2 * element + 2) <= (len(self.vals) - 1) and direction(2):
+                child = 1 if right_child() else 2
+            elif direction(1):
+                child = 1
+            else:
+                break
 
-    while element > 0 and direction():
-        temp = heap[element]
-        heap[element] = heap[(element - 1) // 2]
-        heap[(element - 1) // 2] = temp
-        element = (element - 1) // 2
+            temp = self.vals[2 * element + child]
+            self.vals[2 * element + child] = self.vals[element]
+            self.vals[element] = temp
+            element = 2 * element + child
+        return item
 
+    def insert_element(self, item):
+        self.vals.extend([item])
+        element = len(self.vals) - 1
 
-def make_heap(arr: list, type: str) -> list:
-    temp = []
-    for i in arr:
-        insert(type, i, temp)
-    return temp
+        def direction() -> bool:
+            if self.type == 'max':
+                return self.vals[(element - 1) // 2] < self.vals[element]
+            if self.type == 'min':
+                return self.vals[(element - 1) // 2] > self.vals[element]
+            raise KeyError('\'type\' must be either \'min\' or \'max\'.')
+
+        while element and direction():
+            temp = self.vals[element]
+            self.vals[element] = self.vals[(element - 1) // 2]
+            self.vals[(element - 1) // 2] = temp
+            element = (element - 1) // 2
+
+    def insert(self, elements: list):
+        for element in elements:
+            self.insert_element(element)
