5adb958898d179e601f917905a4dfeed648afa0d
Kadeem <kadbaldwin@gmail.com>
2020-09-01 02:56:10 -0500

    Created Tree Node and Binary Search Tree

---
+++ DataStructures/binary_search_tree.py
@@ -0,0 +1,69 @@
+from DataStructures.AbstractDataTypes import tree_node
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def insert(self, item, node):
+        if node is None:
+            self.root = tree_node.Node(item, None, None)
+        elif item is node.val:
+            raise ValueError('Node already exists!')
+        elif node.val > item:
+            if node.left is None:
+                node.left = tree_node.Node(item, None, None)
+            else:
+                self.insert(item, node.left)
+        else:
+            if node.right is None:
+                node.right = tree_node.Node(item, None, None)
+            else:
+                self.insert(item, node.right)
+
+    def remove(self, item, root):
+        if root.val > item:
+            root.left = self.remove(item, root.left)
+        elif root.val < item:
+            root.right = self.remove(item, root.right)
+        else:
+            if root.left is None:
+                temp = root.right
+                del root
+                return temp
+            if root.right is None:
+                temp = root.left
+                del root
+                return temp
+            temp = root.right
+            while temp.left is not None:
+                temp = temp.left
+            root.val = temp.val
+            del temp
+        return root
+
+    def search(self, item, node=None, traversal='inorder'):
+        if node is None:
+            return None
+        if traversal == 'preorder' and node.val is not item:
+            print(node.val)
+            if item < node.val:
+                self.search(item, node.left, traversal)
+            else:
+                self.search(item, node.right, traversal)
+        elif traversal == 'inorder' and node.val is not item:
+            if item < node.val:
+                self.search(item, node.left, traversal)
+            print(node.val)
+            if item > node.val:
+                self.search(item, node.right, traversal)
+        elif traversal == 'postorder' and node.val is not item:
+            if item < node.val:
+                self.search(item, node.left, traversal)
+            else:
+                self.search(item, node.right, traversal)
+            print(node.val)
+        else:
+            print(node.val)
+            return node
--- DataStructures/tree.py
+++
@@ -1,4 +0,0 @@
-class Node:
-    def __init__(self, val=0, node=None):
-        self.val = val
-        self.node = node
