0c38aa0552d0bcf836ee97dffba8168df66a3455
Kadeem <kadbaldwin@gmail.com>
2020-09-14 16:49:42 -0500

    various updates.

--- DataStructures/disjoint_set.py
+++ DataStructures/disjoint_set.py
@@ -2,20 +2,20 @@ class DisjointSet:
     def __init__(self, graph: dict):
         self.graph = graph
 
-    def count(self, nodes: int) -> int:
-        return [row[0] for row in list(self.graph.values())].count(nodes)
+    def count(self, value: int) -> int:
+        return [row[1] for row in list(self.graph.values())].count(value)
 
-    def find(self, node: str) -> int:
-        root = self.graph[node][1]
-        while root != self.graph[root][1]:
-            root = self.graph[root][1]
+    def find(self, node):
+        root = self.graph[node][0]
+        while root != self.graph[root][0]:
+            root = self.graph[root][0]
         return root
 
-    def union(self, pair: list):
+    def union(self, pair):
         root_a = self.find(pair[0])
         root_b = self.find(pair[1])
         if root_a != root_b:
-            if self.count(self.graph[root_a][0]) <= self.count(self.graph[root_b][0]):
+            if self.count(self.graph[root_a][1]) <= self.count(self.graph[root_b][1]):
                 self.graph[root_b] = self.graph[root_a]
                 self.graph[pair[1]] = self.graph[root_a]  # path compression
             else:
