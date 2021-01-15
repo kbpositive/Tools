ceb8ff83da896c207773b66dcf0ac2d211127b30
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-01-15 01:04:07 -0600

    various refactorings and added tests

--- DataStructures/List/linked.py
+++ DataStructures/List/linked.py
@@ -18,19 +18,6 @@ class List:
 
         return listToPrint
 
-    def insert(self, item):
-        current = Node(item, self.head)
-
-        while current.next:
-            current = current.next
-
-        if current.next != self.head:
-            current.next = Node(item, current.next)
-        else:
-            self.head = current
-
-        self.size += 1
-
     def insert_at_index(self, item, index):
         current = Node(item, self.head)
 
@@ -54,7 +41,7 @@ class List:
         while current.next and current.next.val != search:
             current = current.next
 
-        if search and current.next.val != search:
+        if search and current.next is None:
             raise Exception("Item not found")
 
         if current.next != self.head:
@@ -64,57 +51,40 @@ class List:
 
         self.size += 1
 
-    def remove(self, pos='last', item=None, indx=None):
-        if self.head is None:
-            raise Exception("Empty list.")
-        else:
-            current = self.head
+    def remove_index(self, index):
+        if self.head:
+            current = Node(None,self.head)
 
-        if item is not None:
-            if item == current.val:
-                self.head = self.head.next
-                self.size -= 1
-                return
+            while current.next and (index > 0):
+                current = current.next
+                index -= 1
+
+            if index != 0:
+                raise Exception("Index out of range.")
+
+            if current.next != self.head:
+                current.next = current.next.next
             else:
-                while current.next:
-                    if item == current.next.val:
-                        current.next = current.next.next
-                        self.size -= 1
-                        return
-                    current = current.next
-            raise Exception ("Item not found.")
-        elif indx is not None:
-            if indx > 0:
-                if current.next:
-                    while current.next.next and indx:
-                        current = current.next
-                        indx -= 1
-                    current.next = current.next.next
-                    self.size -= 1
-                    return
-                else:
-                    raise Exception("Index out of range.")
+                self.head = current.next.next
+
+            self.size -= 1
+
+    def remove_item(self, search):
+        if self.head:
+            current = Node(None,self.head)
+
+            while current.next and current.next.val != search:
+                current = current.next
+
+            if current.next is None or current.next.val != search:
+                raise Exception("Item not found")
+
+            if current.next != self.head:
+                current.next = current.next.next
             else:
-                self.head = self.head.next
-        else:
-            if pos == 'last':
-                if current.next:
-                    if current.next.next:
-                        while current.next.next:
-                            current = current.next
-                        current.next = None
-                        self.size -= 1
-                        return
-                    else:
-                        self.head.next = None
-                else:
-                    self.head = None
-            elif pos == 'first':
-                self.head = self.head.next
-                self.size -= 1
-                return
-            else:
-                raise Exception("Invalid position. Please use 'first' or 'last'.")
+                self.head = current.next.next
+
+            self.size -= 1
 
     @property
     def length(self):
@@ -125,7 +95,7 @@ if __name__ == '__main__':
     items = [3,2,1,9,8,7,6,5,4,0]
 
     for item in items:
-        L.insert(item)
+        L.insert_at_index(item,L.length)
 
     assert L.print_list() == [3,2,1,9,8,7,6,5,4,0]
     assert L.length == 10
@@ -147,7 +117,7 @@ if __name__ == '__main__':
 
     G = List()
 
-    G.insert_at_item(3,0)
+    G.insert_at_item(3,None)
     G.insert_at_item(5,3)
     G.insert_at_item(7,5)
     G.insert_at_item(4,3)
@@ -162,10 +132,18 @@ if __name__ == '__main__':
     assert G.length == 5
     assert L.length == 16
 
-    L.remove()
-    L.remove(item=7)
-    L.remove(indx=5)
-    assert L.print_list() == [2,4,3,2,0,0,1,9,8,6,5,5,4]
-    assert L.size == 13
+    L.remove_item(7)
+    L.remove_item(6)
+    L.remove_item(0)
+    L.remove_item(1)
 
+    assert L.print_list() == [2,4,3,2,0,1,9,8,5,5,4,0]
+    assert L.length == 12
+
+    for i in range(3):
+        L.remove_index(i)
+
+    assert L.print_list() == [4,2,1,9,8,5,5,4,0]
+    assert L.length == 9
+    
     print("Pass")
