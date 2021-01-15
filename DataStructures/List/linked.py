07033b4f691022f524a4e9edf8d950a0428de208
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-01-14 23:31:46 -0600

    refactored insert method into insert, insert_at_index, and insert_at_item methods
    added tests

--- DataStructures/List/linked.py
+++ DataStructures/List/linked.py
@@ -1,99 +1,68 @@
 class Node:
-    def __init__(self, val, nxt=None):
+    def __init__(self, val, next=None):
         self.val = val
-        self.nxt = nxt
+        self.next = next
 
 class List:
-    def __init__(self, head=None):
+    def __init__(self, head=None, size=0):
         self.head = head
-        self.size = 0
-        if self.head is not None:
-            self.traverse()
+        self.size = size
 
-    def traverse(self):
-        if self.head is None:
-            raise Exception("Empty list.")
-        else:
-            self.size = 0
-            output = []
-            current = self.head
-            while current:
-                output.append(current.val)
-                self.size += 1
-                current = current.nxt
-        return output
+    def print_list(self):
+        listToPrint = []
+        current = self.head
 
-    def insert(self, val, pos='after', item=None, indx=None):
-        node = Node(val)
-        if self.head:
-            current = self.head
-        else:
-            self.head = node
-            self.size = 1
-            return
+        while current:
+            listToPrint.append(current.val)
+            current = current.next
 
-        if pos == 'after':
-            if item is not None:
-                while current:
-                    if item == current.val:
-                        node.nxt = current.nxt
-                        current.nxt = node
-                        self.size += 1
-                        return
-                    current = current.nxt
-                raise Exception("Item not found.")
-            elif indx is not None:
-                while current and indx:
-                    current = current.nxt
-                    indx -= 1
-                if current:
-                    node.nxt = current.nxt
-                    current.nxt = node
-                    self.size += 1
-                    return
-                raise Exception("Index out of range.")
-            else:
-                while current.nxt:
-                    current = current.nxt
-                current.nxt = node
-                self.size += 1
-                return
-        elif pos == 'before':
-            if item is not None:
-                if item == current.val:
-                    self.head = Node(val,self.head)
-                    self.size += 1
-                    return
-                elif current.nxt:
-                    while current.nxt:
-                        if current.nxt.val is item:
-                            node.nxt = current.nxt
-                            current.nxt = node
-                            self.size += 1
-                            return
-                        current = current.nxt
-                raise Exception("Item not found.")
-            elif indx is not None:
-                if indx == 0:
-                    self.head = Node(val,self.head)
-                    self.size += 1
-                    return
-                elif current.nxt:
-                    indx -= 1
-                    while current.nxt and indx:
-                        current = current.nxt
-                        indx -= 1
-                    if current.nxt:
-                        current.nxt = Node(val,current.nxt)
-                        self.size += 1
-                        return
-                raise Exception("Index out of range.")
-            else:
-                self.head = Node(val,self.head)
-                self.size += 1
-                return
+        return listToPrint
+
+    def insert(self, item):
+        current = Node(item, self.head)
+
+        while current.next:
+            current = current.next
+
+        if current.next != self.head:
+            current.next = Node(item, current.next)
         else:
-            raise Exception("Invalid position. Please use 'before' or 'after'.")
+            self.head = current
+
+        self.size += 1
+
+    def insert_at_index(self, item, index):
+        current = Node(item, self.head)
+
+        while current.next and (index > 0):
+            current = current.next
+            index -= 1
+
+        if index != 0:
+            raise Exception("Index out of range.")
+
+        if current.next != self.head:
+            current.next = Node(item, current.next)
+        else:
+            self.head = current
+
+        self.size += 1
+
+    def insert_at_item(self, item, search):
+        current = Node(item, self.head)
+
+        while current.next and current.next.val != search:
+            current = current.next
+
+        if search and current.next.val != search:
+            raise Exception("Item not found")
+
+        if current.next != self.head:
+            current.next = Node(item, current.next)
+        else:
+            self.head = current
+
+        self.size += 1
 
     def remove(self, pos='last', item=None, indx=None):
         if self.head is None:
@@ -103,68 +72,100 @@ class List:
 
         if item is not None:
             if item == current.val:
-                self.head = self.head.nxt
+                self.head = self.head.next
                 self.size -= 1
                 return
             else:
-                while current.nxt:
-                    if item == current.nxt.val:
-                        current.nxt = current.nxt.nxt
+                while current.next:
+                    if item == current.next.val:
+                        current.next = current.next.next
                         self.size -= 1
                         return
-                    current = current.nxt
+                    current = current.next
             raise Exception ("Item not found.")
         elif indx is not None:
             if indx > 0:
-                if current.nxt:
-                    while current.nxt.nxt and indx:
-                        current = current.nxt
+                if current.next:
+                    while current.next.next and indx:
+                        current = current.next
                         indx -= 1
-                    current.nxt = current.nxt.nxt
+                    current.next = current.next.next
                     self.size -= 1
                     return
                 else:
                     raise Exception("Index out of range.")
             else:
-                self.head = self.head.nxt
+                self.head = self.head.next
         else:
             if pos == 'last':
-                if current.nxt:
-                    if current.nxt.nxt:
-                        while current.nxt.nxt:
-                            current = current.nxt
-                        current.nxt = None
+                if current.next:
+                    if current.next.next:
+                        while current.next.next:
+                            current = current.next
+                        current.next = None
                         self.size -= 1
                         return
                     else:
-                        self.head.nxt = None
+                        self.head.next = None
                 else:
                     self.head = None
             elif pos == 'first':
-                self.head = self.head.nxt
+                self.head = self.head.next
                 self.size -= 1
                 return
             else:
                 raise Exception("Invalid position. Please use 'first' or 'last'.")
 
+    @property
+    def length(self):
+        return self.size
+
 if __name__ == '__main__':
     L = List()
     items = [3,2,1,9,8,7,6,5,4,0]
+
     for item in items:
         L.insert(item)
 
-    L.insert(8,'after',7)
-    L.insert(0,'after',indx=2)
-    L.insert(4,'before',item=7)
-    L.insert(5,'before',indx=5)
-    assert L.traverse() == [3, 2, 1, 0, 9, 5, 8, 4, 7, 8, 6, 5, 4, 0]
-    assert L.size == 14
+    assert L.print_list() == [3,2,1,9,8,7,6,5,4,0]
+    assert L.length == 10
+
+    G = List()
+
+    G.insert_at_index(3,0)
+    G.insert_at_index(2,1)
+    G.insert_at_index(1,0)
+
+    L.insert_at_index(0, 2)
+    L.insert_at_index(2, 0)
+    L.insert_at_index(4, 1)
+
+    assert G.print_list() == [1,3,2]
+    assert L.print_list() == [2,4,3,2,0,1,9,8,7,6,5,4,0]
+    assert G.length == 3
+    assert L.length == 13
+
+    G = List()
+
+    G.insert_at_item(3,0)
+    G.insert_at_item(5,3)
+    G.insert_at_item(7,5)
+    G.insert_at_item(4,3)
+    G.insert_at_item(9,5)
+
+    L.insert_at_item(5, 5)
+    L.insert_at_item(0, 0)
+    L.insert_at_item(1, 1)
+
+    assert G.print_list() == [7,9,5,4,3]
+    assert L.print_list() == [2,4,3,2,0,0,1,1,9,8,7,6,5,5,4,0]
+    assert G.length == 5
+    assert L.length == 16
 
     L.remove()
-    L.remove('first')
     L.remove(item=7)
     L.remove(indx=5)
-    assert L.traverse() == [2, 1, 0, 9, 5, 8, 8, 6, 5, 4]
-    assert L.size == 10
+    assert L.print_list() == [2,4,3,2,0,0,1,9,8,6,5,5,4]
+    assert L.size == 13
 
     print("Pass")
