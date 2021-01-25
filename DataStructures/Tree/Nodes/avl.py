d9fc99b9642083c9fd1ef05549977e47818841fe
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-01-25 12:33:35 -0600

    removed old files

--- DataStructures/Tree/Nodes/avl.py
+++
@@ -1,8 +0,0 @@
-class Node:
-	def __init__(self, parent=None, val=0, left=None, right=None):
-		self.parent = parent
-		self.val = val
-		self.left = left
-		self.right = right
-		self.height = 0
-		self.bf = 0
