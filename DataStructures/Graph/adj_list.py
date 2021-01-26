1f823537dc6722efa26e608d86a6eff4dc9c5415
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-01-25 23:56:55 -0600

    increased graph functionality

--- DataStructures/Graph/adj_list.py
+++ DataStructures/Graph/adj_list.py
@@ -1,38 +1,36 @@
 class Graph:
-    def __init__(self, vertices, edges):
+    def __init__(self, edges):
         self.edges = set(frozenset((a,b)) for a,b in edges)
-        self.neighbors = {}
-        for vertex in vertices:
-            self.addVertex(vertex)
+        self.vertices = {}
         for a,b in self.edges:
             self.addEdge(a,b)
+        for vertex in self.vertices:
+            self.add_vertex(vertex)
 
-    def addVertex(self, vertex):
-        if vertex not in self.neighbors:
-            self.neighbors[vertex] = set()
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = {}
 
     def addEdge(self, a, b):
-        self.addVertex(a)
-        self.addVertex(b)
+        self.add_vertex(a)
+        self.add_vertex(b)
         self.edges.add(frozenset([a,b]))
-        self.neighbors[a].add(b)
-        self.neighbors[b].add(a)
+        self.vertices[a][b] = self.vertices[b]
+        self.vertices[b][a] = self.vertices[a]
 
-    def removeVertex(self, vertex):
-        neighborList = list(self.neighbors[vertex])
-        for neighbor in neighborList:
-            self.removeEdge(vertex, neighbor)
-        del self.neighbors[vertex]
+    def removeVertex(self, item):
+        for vertex, neighbor in self.vertices.items():
+            if item in neighbor:
+                del neighbor[item]
+                self.edges.remove(frozenset([vertex,item]))
+        del self.vertices[item]
 
     def removeEdge(self, a, b):
         edge = frozenset([a,b])
         if edge in self.edges:
             self.edges.remove(edge)
-            self.neighbors[a].remove(b)
-            self.neighbors[b].remove(a)
-
-    def getNeighbors(self, vertex):
-        return set(self.neighbors[vertex])
+            del self.vertices[a][b]
+            del self.vertices[b][a]
 
     @property
     def m(self):
@@ -40,35 +38,63 @@ class Graph:
 
     @property
     def n(self):
-        return len(self.neighbors)
+        return len(self.vertices)
 
 if __name__ == '__main__':
-    G = Graph(
-        [1,1,2,3,7,6,6,4,8,5],
-        {(1,2),(2,3),(6,7),(2,3),(8,3),(2,6),(8,4),(5,1)})
+    G = Graph({(1,2),(1,3),(1,4),(2,3),(4,3),(3,5),(4,5)})
 
-    assert G.getNeighbors(3) == {8,2}
+    def print_graph(graph):
+        for vertex,neighbors in graph.items():
+            print(str(vertex) + " :", end=" ")
+            for neighbor in neighbors:
+                print(str(neighbor), end=" ")
+            print("")
+
+
+
+    #print_graph(G.vertices)
+
     assert G.m == 7
-    assert G.n == 8
+    assert G.n == 5
 
     G.removeEdge(2,3)
     assert G.m == 6
-    assert G.n == 8
+    assert G.n == 5
 
-    G.removeEdge(2,3)
-    assert G.m == 6
-    assert G.n == 8
+    G.removeEdge(1,4)
+    assert G.m == 5
+    assert G.n == 5
 
     G.addEdge(2,3)
+    assert G.m == 6
+    assert G.n == 5
+
+    G.addEdge(3,9)
     assert G.m == 7
-    assert G.n == 8
+    assert G.n == 6
 
     G.removeVertex(3)
-    assert G.m == 5
-    assert G.n == 7
+    assert G.m == 2
+    assert G.n == 5
 
-    G.addVertex(9)
+    G.addEdge(3,9)
+    assert G.m == 3
+    assert G.n == 6
+
+    G.addEdge(1,3)
+    assert G.m == 4
+    assert G.n == 6
+
+    G.addEdge(5,3)
     assert G.m == 5
-    assert G.n == 8
+    assert G.n == 6
+
+    G.add_vertex(9)
+    assert G.m == 5
+    assert G.n == 6
+
+    #print_graph(G.vertices)
+
+    assert list(G.vertices[5][4][5][3][1][2][1][3][9].keys())[0] == 3
 
     print('Pass')
