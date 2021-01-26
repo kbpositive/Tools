c98ab0630a390e3e816949033684b384be2d7b75
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-01-25 23:57:12 -0600

    started adjacency matrix graph

--- DataStructures/Graph/adj_matrix.py
+++ DataStructures/Graph/adj_matrix.py
@@ -1,3 +1,19 @@
 class Graph:
-    def __init__(self, weighted_edges):
-        self.edges = weighted_edges
+    def __init__(self, edges):
+        self.edges = set(frozenset((a,b,c)) for a,b,c in edges)
+
+
+    def add_edge(self, a, b):
+        return
+
+    def add_vertex(self, v):
+        return
+
+    def print_graph():
+        return
+
+
+if __name__ == '__main__':
+    G = Graph({(1,2,2),(1,3,2),(1,4,2),(2,3,2),(4,3,2),(3,5,2),(4,5,2)})
+
+    print("pass")
