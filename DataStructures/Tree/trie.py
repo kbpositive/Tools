3593e330d0c4bf0ceb89db6b02b3fe654d4faa20
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-02-06 18:29:50 -0600

    created more legible variable names

--- DataStructures/Tree/trie.py
+++ DataStructures/Tree/trie.py
@@ -1,7 +1,7 @@
 class Node:
-    def __init__(self, val="", alphabet_length=26):
-        self.val = val
-        self.letter = [None]*alphabet_length
+    def __init__(self, symbol="", alphabet_length=26):
+        self.symbol = symbol
+        self.letters = [None]*alphabet_length
         self.end = False
 
 class Trie:
@@ -12,29 +12,29 @@ class Trie:
     def add(self,trie,word):
         if trie:
             if word:
-                if trie.letter[ord(word[0])-97] and len(word) > 1:
-                    self.add(trie.letter[ord(word[0])-97],word[1:])
-                elif trie.letter[ord(word[0])-97] is None:
-                    trie.letter[ord(word[0])-97] = Node(word[0])
-                    self.add(trie.letter[ord(word[0])-97],word[1:])
+                if trie.letters[ord(word[0])-97] and len(word) > 1:
+                    self.add(trie.letters[ord(word[0])-97],word[1:])
+                elif trie.letters[ord(word[0])-97] is None:
+                    trie.letters[ord(word[0])-97] = Node(word[0])
+                    self.add(trie.letters[ord(word[0])-97],word[1:])
                 else:
-                    trie.letter[ord(word[0])-97].val = word[0]
-                    trie.letter[ord(word[0])-97].end = True
+                    trie.letters[ord(word[0])-97].symbol = word[0]
+                    trie.letters[ord(word[0])-97].end = True
             else:
                 trie.end = True
 
     def print_trie(self,node,count):
         if node:
-            for index,key in enumerate([l for l in node.letter if l]):
+            for index,key in enumerate([l for l in node.letters if l]):
                 if index > 0:
                     print('  '*count,end='')
-                print(key.val,end='')
+                print(key.symbol,end='')
                 if key.end == False:
                     print('-',end='')
                 else:
                     print('\n',end='')
-                    if key.letter != [None]*len(self.alphabet):
-                        print('  '*count+key.val,end='-')
+                    if key.letters != [None]*len(self.alphabet):
+                        print('  '*count+key.symbol,end='-')
 
                 self.print_trie(key,count+1)
 
@@ -42,14 +42,16 @@ if __name__ == '__main__':
     tr = Trie()
 
     tr.add(tr.root,'test')
-    traverse_test = tr.root.letter[ord('t')-97].letter[ord('e')-97].letter[ord('s')-97].letter[ord('t')-97]
-    assert traverse_test.letter == [None]*26
+    traverse_test = tr.root.letters[ord('t')-97].letters[ord('e')-97].letters[ord('s')-97].letters[ord('t')-97]
+    assert traverse_test.letters == [None]*26
     assert traverse_test.end == True
 
     tr.add(tr.root,'taxes')
     tr.add(tr.root,'harry')
     tr.add(tr.root,'potter')
     tr.add(tr.root,'taste')
+    tr.add(tr.root,'tasty')
+    tr.add(tr.root,'tasteless')
     tr.add(tr.root,'teacher')
     tr.add(tr.root,'parasaur')
     tr.add(tr.root,'general')
@@ -58,6 +60,6 @@ if __name__ == '__main__':
     tr.add(tr.root,'generalizational')
     tr.add(tr.root,'generalizationalize')
     tr.add(tr.root,'generalizationalization')
-    
+
     tr.print_trie(tr.root,0)
     print('pass')
