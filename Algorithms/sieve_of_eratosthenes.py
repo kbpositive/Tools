d41a4c692c124391f3ee145b725fce1aa19f6579
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-03-10 14:53:32 -0600

    refactored sieve function and added an example

--- Algorithms/sieve_of_eratosthenes.py
+++ Algorithms/sieve_of_eratosthenes.py
@@ -1,10 +1,11 @@
-def nats(n):
-    yield n
-    yield from nats(n + 1)
+def seq(n):
+    while True:
+        yield n
+        n += 1
 
+sieve = lambda x,y,z: y if x == 0 else sieve(x-1,next(z),(i for i in z if i % y != 0))
 
-def sieve(s):
-    n = next(s)
-    yield n
-    yield from sieve(i for i in s if i % n != 0)
-
+if __name__ == '__main__':
+    assert sieve(1000,2,seq(2)) == 7919
+    print('\npass')
+    
