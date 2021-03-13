73ed74f5020e8731b153abc770c8858fe7cc8932
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-03-12 19:24:18 -0600

    set defaults for sieve for efficiency

--- Algorithms/sieve_of_eratosthenes.py
+++ Algorithms/sieve_of_eratosthenes.py
@@ -3,9 +3,8 @@ def seq(n):
         yield n
         n += 1
 
-sieve = lambda x,y,z: y if x == 0 else sieve(x-1,next(z),(i for i in z if i % y != 0))
+sieve = lambda x,y=2,z=seq(2): y if x == 0 else sieve(x-1,next(z),(i for i in z if i % y != 0))
 
 if __name__ == '__main__':
-    assert sieve(1000,2,seq(2)) == 7919
+    assert sieve(1000) == 7919
     print('\npass')
-    
