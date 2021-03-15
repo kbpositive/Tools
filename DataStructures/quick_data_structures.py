6ac22ba18bd37cd671513ccea616dd0118f9de70
Kadeem Baldwin <kadbaldwin@gmail.com>
2021-03-15 12:08:10 -0500

    converted vertices to set rather than dict

--- DataStructures/quick_data_structures.py
+++ DataStructures/quick_data_structures.py
@@ -7,10 +7,10 @@ def node(data): return [data, {0:None, 1:None}]
 def letter(data): return [data,[None]*128]
 
 def vertices(n):
-    return {num:index for index,num in enumerate(random.sample(range(n),k=n))}
+    return set(random.sample(range(n),k=n))
 
 def edges(v,n,acyclic=True):
-    return [random.sample(v.keys(),k=2) if acyclic else random.choices(list(v.keys()),k=2) for _ in range(n)]
+    return [random.sample(list(v),k=2) if acyclic else random.choices(list(v),k=2) for _ in range(n)]
 
 def linked_list(n):
     head = link(-1)
@@ -47,27 +47,28 @@ def trie(words):
     return root
 
 def adj_list(v,e,directed=False,acyclic=True):
-    verts = vertices(v)
+    verts = {vertex:set() for vertex in vertices(v)}
     edgs = edges(verts,e,acyclic)
-    adj_list = {}
 
     frm = 0
     to = 1
 
     for pair in edgs:
         for vertex in pair:
-            if vertex not in adj_list:
-                adj_list[vertex] = {}
-        adj_list[pair[frm]][pair[to]] = adj_list[pair[to]]
+            if vertex not in verts:
+                verts[vertex] = set()
+
+        verts[pair[frm]].add(pair[to])
         if not directed:
-            adj_list[pair[to]][pair[frm]] = adj_list[pair[frm]]
-    return adj_list
+            verts[pair[to]].add(pair[frm])
+
+    return verts
 
 def adj_mat(v,e,directed=False,weighted=False,acyclic=True):
-    verts = vertices(v)
+    verts = list(vertices(v))
     edge_sets = zip(edges(verts,e,acyclic), random.sample(range(e),k=e))
     edgs = [[edge_set[0][0],edge_set[0][1],edge_set[1]] for edge_set in edge_sets]
-    adj_mat = [[0]*v for row in range(v)]
+    mat = [[0]*v for row in range(v)]
 
     frm = 0
     to = 1
@@ -75,14 +76,15 @@ def adj_mat(v,e,directed=False,weighted=False,acyclic=True):
     for triplet in edgs:
         for vertex in triplet[:2]:
             if vertex not in verts:
-                verts[vertex] = len(adj_mat)
-                adj_mat.append([0]*len(adj_mat))
-                for row in adj_mat:
+                verts[vertex] = len(mat)
+                mat.append([0]*len(mat))
+                for row in mat:
                     row.append(0)
-        adj_mat[verts[triplet[frm]]][verts[triplet[to]]] = 1*(triplet[2]**int(weighted))
-        if adj_mat[verts[triplet[to]]][verts[triplet[frm]]] == 0:
-            adj_mat[verts[triplet[to]]][verts[triplet[frm]]] = (-1*(triplet[2]**int(weighted)))**int(directed)
-    return adj_mat
+
+        mat[verts[triplet[frm]]][verts[triplet[to]]] = 1*(triplet[2]**int(weighted))
+        if mat[verts[triplet[to]]][verts[triplet[frm]]] == 0:
+            mat[verts[triplet[to]]][verts[triplet[frm]]] = (-1*(triplet[2]**int(weighted)))**int(directed)
+    return mat
 
 if __name__ == '__main__':
     # linked list
