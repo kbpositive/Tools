7d17381ff2784f7681f40cd93f46c91e5d4a4dc9
kbpositive <kadbaldwin@gmail.com>
2021-04-09 14:30:49 -0500

    reformatted

--- DataStructures/quick_data_structures.py
+++ DataStructures/quick_data_structures.py
@@ -1,36 +1,50 @@
 import random
 
-def link(data): return [data, None]
 
-def node(data): return [data, [None, None]]
+def link(data):
+    return [data, None]
 
-def letter(data): return [data,[None]*128]
+
+def node(data):
+    return [data, [None, None]]
+
+
+def letter(data):
+    return [data, [None] * 128]
+
 
 def vertices(n):
-    return set(random.sample(range(n),k=n))
+    return set(random.sample(range(n), k=n))
 
-def edges(v,n,acyclic=True):
-    return [random.sample(list(v),k=2) if acyclic else random.choices(list(v),k=2) for _ in range(n)]
+
+def edges(v, n, acyclic=True):
+    return [
+        random.sample(list(v), k=2) if acyclic else random.choices(list(v), k=2)
+        for _ in range(n)
+    ]
+
 
 def linked_list(n):
     head = link(-1)
     current = head
     nxt = 1
 
-    for next_num in random.sample(range(n),k=n):
+    for next_num in random.sample(range(n), k=n):
         current[nxt] = link(next_num)
         current = current[nxt]
     return head
 
+
 def tree(n):
     root = node(-1)
 
-    for next_num in random.sample(range(n),k=n):
+    for next_num in random.sample(range(n), k=n):
         current = root
         while current[1][current[0] < next_num]:
             current = current[1][current[0] < next_num]
         current[1][current[0] < next_num] = node(next_num)
     return root
+
 
 def trie(words):
     root = letter("")
@@ -44,9 +58,10 @@ def trie(words):
             current = current[nxt][ord(char)]
     return root
 
-def adj_list(v,e,directed=False,acyclic=True):
-    verts = {vertex:set() for vertex in vertices(v)}
-    edgs = edges(verts,e,acyclic)
+
+def adj_list(v, e, directed=False, acyclic=True):
+    verts = {vertex: set() for vertex in vertices(v)}
+    edgs = edges(verts, e, acyclic)
 
     frm = 0
     to = 1
@@ -62,11 +77,12 @@ def adj_list(v,e,directed=False,acyclic=True):
 
     return verts
 
-def adj_mat(v,e,directed=False,weighted=False,acyclic=True):
+
+def adj_mat(v, e, directed=False, weighted=False, acyclic=True):
     verts = list(vertices(v))
-    edge_sets = zip(edges(verts,e,acyclic), random.sample(range(e),k=e))
-    edgs = [[edge_set[0][0],edge_set[0][1],edge_set[1]] for edge_set in edge_sets]
-    mat = [[0]*v for row in range(v)]
+    edge_sets = zip(edges(verts, e, acyclic), random.sample(range(e), k=e))
+    edgs = [[edge_set[0][0], edge_set[0][1], edge_set[1]] for edge_set in edge_sets]
+    mat = [[0] * v for row in range(v)]
 
     frm = 0
     to = 1
@@ -75,29 +91,32 @@ def adj_mat(v,e,directed=False,weighted=False,acyclic=True):
         for vertex in triplet[:2]:
             if vertex not in verts:
                 verts[vertex] = len(mat)
-                mat.append([0]*len(mat))
+                mat.append([0] * len(mat))
                 for row in mat:
                     row.append(0)
 
-        mat[verts[triplet[frm]]][verts[triplet[to]]] = 1*(triplet[2]**int(weighted))
+        mat[verts[triplet[frm]]][verts[triplet[to]]] = 1 * (triplet[2] ** int(weighted))
         if mat[verts[triplet[to]]][verts[triplet[frm]]] == 0:
-            mat[verts[triplet[to]]][verts[triplet[frm]]] = (-1*(triplet[2]**int(weighted)))**int(directed)
+            mat[verts[triplet[to]]][verts[triplet[frm]]] = (
+                -1 * (triplet[2] ** int(weighted))
+            ) ** int(directed)
     return mat
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # linked list
-    print(linked_list(7),end='\n\n')
+    print(linked_list(7), end="\n\n")
 
     # tree
-    print(tree(25))
+    print(tree(5))
 
     # trie
-    print(trie(["lions","tigers","bears"]))
+    print(trie(["lions", "tigers", "bears"]))
 
     # graph: adjacency list
-    print(adj_list(9,10,directed=True,acyclic=False))
+    print(adj_list(9, 10, directed=True, acyclic=False))
 
     # graph: adjacency matrix
-    print(adj_mat(10,20,weighted=True,directed=True,acyclic=False))
+    print(adj_mat(10, 20, weighted=True, directed=True, acyclic=False))
 
     print("\npass")
