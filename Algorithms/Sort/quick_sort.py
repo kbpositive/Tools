2c187693bd653ba3f43b95aa99872854a8c5bc89
kbpositive <kadbaldwin@gmail.com>
2021-06-12 02:44:20 -0500

    refactorings

--- Algorithms/Sort/quick_sort.py
+++ Algorithms/Sort/quick_sort.py
@@ -1,8 +1,11 @@
 def partition(arr: list, pivot: int, left: int, right: int) -> int:
     while left <= right:
         left = next(ind for ind, val in enumerate(arr[left:], left) if val >= pivot)
-        while arr[right] > pivot:
-            right -= 1
+        right = next(
+            -ind
+            for ind, val in enumerate(arr[: right + 1][::-1], -right)
+            if val <= pivot
+        )
         if left <= right:
             arr[left], arr[right] = arr[right], arr[left]
             left += 1
@@ -23,4 +26,6 @@ def sort(arr: list) -> list:
 
 
 if __name__ == "__main__":
-    assert sort([6, 5, 7, 4, 8, 3, 9, 2, 1]) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    import random
+
+    assert sort(random.sample(range(20), k=20)) == list(range(20))
