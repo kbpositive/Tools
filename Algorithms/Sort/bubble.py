cab2e5ab3d2d2aa4c867d6f5abf55ba604421a31
kbpositive <kadbaldwin@gmail.com>
2021-06-12 03:45:59 -0500

    removed redundant function

--- Algorithms/Sort/bubble.py
+++ Algorithms/Sort/bubble.py
@@ -1,27 +1,13 @@
 def sort(arr):
-    arr_copy = arr[:]
     for _ in range(len(arr)):
-        for indx in range(len(arr_copy)-1):
-            arr_copy[indx:indx+2] = arr_copy[indx:indx+2][::(-1)**((arr_copy[indx] < arr_copy[indx+1])+1)]
-    return arr_copy
-
-def sort_in_place(arr):
-    for _ in range(len(arr)):
-        for indx in range(len(arr)-1):
-            arr[indx:indx+2] = arr[indx:indx+2][::(-1)**((arr[indx] < arr[indx+1])+1)]
+        for indx in range(len(arr) - 1):
+            arr[indx : indx + 2] = arr[indx : indx + 2][
+                :: (-1) ** ((arr[indx] < arr[indx + 1]) + 1)
+            ]
     return arr
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import random
 
-    arr = random.sample(range(10),k=10)
-
-    mem = arr[:]
-
-    assert sort(arr) == list(range(10))
-
-    assert mem == arr
-
-    assert sort_in_place(arr) == list(range(10))
-
-    assert arr == list(range(10))
+    assert sort(random.sample(range(10), k=10)) == list(range(10))
