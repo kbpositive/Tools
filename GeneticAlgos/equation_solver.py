aaf58530b00f1a3c044e54f7450065f317e12ca5
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-07-29 05:06:31 -0500

    created organism evaluator

--- GeneticAlgos/equation_solver.py
+++ GeneticAlgos/equation_solver.py
@@ -2,18 +2,47 @@ import operator
 import random
 
 genome = {
-    "0": [0, [0, 0, 0, 0, 0]],
-    "1": [1, [0, 0, 0, 0, 1]],
-    "2": [2, [0, 0, 0, 1, 0]],
-    "3": [3, [0, 0, 0, 1, 1]],
-    "4": [4, [0, 0, 1, 0, 0]],
-    "5": [5, [0, 0, 1, 0, 1]],
-    "6": [6, [0, 0, 1, 1, 0]],
-    "7": [7, [0, 0, 1, 1, 1]],
-    "8": [8, [0, 1, 0, 0, 0]],
-    "9": [9, [0, 1, 0, 0, 1]],
-    "+": [operator.add, [0, 1, 0, 1, 0]],
-    "-": [operator.sub, [0, 1, 0, 1, 1]],
-    "*": [operator.mul, [0, 1, 1, 0, 0]],
-    "/": [operator.truediv, [0, 1, 1, 0, 1]],
+    "0": [0.0, [0.0, 0.0, 0.0, 0.0, 0.0]],
+    "1": [1.0, [0.0, 0.0, 0.0, 0.0, 1.0]],
+    "2": [2.0, [0.0, 0.0, 0.0, 1.0, 0.0]],
+    "3": [3.0, [0.0, 0.0, 0.0, 1.0, 1.0]],
+    "4": [4.0, [0.0, 0.0, 1.0, 0.0, 0.0]],
+    "5": [5.0, [0.0, 0.0, 1.0, 0.0, 1.0]],
+    "6": [6.0, [0.0, 0.0, 1.0, 1.0, 0.0]],
+    "7": [7.0, [0.0, 0.0, 1.0, 1.0, 1.0]],
+    "8": [8.0, [0.0, 1.0, 0.0, 0.0, 0.0]],
+    "9": [9.0, [0.0, 1.0, 0.0, 0.0, 1.0]],
+    "+": [operator.add, [0.0, 1.0, 0.0, 1.0, 0.0]],
+    "-": [operator.sub, [0.0, 1.0, 0.0, 1.0, 1.0]],
+    "*": [operator.mul, [0.0, 1.0, 1.0, 0.0, 0.0]],
+    "/": [operator.truediv, [0.0, 1.0, 1.0, 0.0, 1.0]],
 }
+
+
+def opEval(op1, oper, op2):
+    return genome[oper][0](float(op1), float(op2))
+
+
+def orgEval(organism):
+    opers = [0.0, []]
+    sign = None
+    for char in organism:
+        if type(genome[char][0]) != float:
+            if not opers[1]:
+                continue
+            elif not sign:
+                sign = genome[char][0]
+                opers[0] = float("".join(opers[1]))
+                opers[1] = []
+                continue
+            opers[0] = sign(opers[0], float("".join(opers[1])))
+            opers[1] = []
+            sign = genome[char][0]
+        else:
+            opers[1].append(char)
+    if opers[1]:
+        opers[0] = sign(opers[0], float("".join(opers[1])))
+    return opers[0]
+
+
+orgEval("/2+32/7-")
