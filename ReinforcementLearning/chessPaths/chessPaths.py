23aa07287d39a2e1835c9fa20ee52fa10f26859d
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-08-07 11:51:28 -0500

    numpy refactorings

--- ReinforcementLearning/chessPaths/chessPaths.py
+++ ReinforcementLearning/chessPaths/chessPaths.py
@@ -9,20 +9,32 @@ import os
 
 class Piece:
     def __init__(self, row, col, board):
-        self.pos = row, col
+        self.pos = np.array([row, col])
         self.rewards = board
 
     def state(self):
-        return self.pos[0] - (len(self.rewards) / 2.0), self.pos[1] - (
-            len(self.rewards[0]) / 2.0
-        )
+        return self.pos - (np.array([len(self.rewards), len(self.rewards[0])]) / 2.0)
 
     def reward(self):
         return self.rewards[self.pos[0]][self.pos[1]]
 
 
 class King(Piece):
-    pass
+    moves = {
+        0: np.array([-1, -1]),
+        1: np.array([-1, 0]),
+        2: np.array([-1, 1]),
+        3: np.array([0, -1]),
+        4: np.array([0, 0]),
+        5: np.array([0, 1]),
+        6: np.array([1, -1]),
+        7: np.array([1, 0]),
+        8: np.array([1, 1]),
+    }
+
+    def move(self, n):
+
+        return self
 
 
 class Knight(Piece):
