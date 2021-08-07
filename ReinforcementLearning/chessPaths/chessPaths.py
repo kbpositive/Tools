a9d4099ac12eb5d0786b5f0c6a920da2d917a9c3
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-08-07 12:52:32 -0500

    added moves and set up state, reward structure

--- ReinforcementLearning/chessPaths/chessPaths.py
+++ ReinforcementLearning/chessPaths/chessPaths.py
@@ -7,42 +7,37 @@ import imageio
 import os
 
 
+class Board:
+    def __init__(self, rewards):
+        self.rewards = rewards
+
+    def reward(self, pos):
+        return self.rewards[pos[0]][pos[1]]
+
+
 class Piece:
-    def __init__(self, row, col, board):
-        self.pos = np.array([row, col])
-        self.rewards = board
+    def __init__(self, pos):
+        self.pos = pos
 
-    def state(self):
-        return self.pos - (np.array([len(self.rewards), len(self.rewards[0])]) / 2.0)
-
-    def reward(self):
-        return self.rewards[self.pos[0]][self.pos[1]]
+    def state(self, board):
+        return self.pos - (np.array([len(board.rewards), len(board.rewards[0])]) / 2.0)
 
 
 class King(Piece):
-    moves = {
-        0: np.array([-1, -1]),
-        1: np.array([-1, 0]),
-        2: np.array([-1, 1]),
-        3: np.array([0, -1]),
-        4: np.array([0, 0]),
-        5: np.array([0, 1]),
-        6: np.array([1, -1]),
-        7: np.array([1, 0]),
-        8: np.array([1, 1]),
-    }
-
-    def move(self, n):
-
-        return self
+    def __init__(self, rewards):
+        self.moves = {
+            0: np.array([-1, -1]),
+            1: np.array([-1, 0]),
+            2: np.array([-1, 1]),
+            3: np.array([0, -1]),
+            4: np.array([0, 0]),
+            5: np.array([0, 1]),
+            6: np.array([1, -1]),
+            7: np.array([1, 0]),
+            8: np.array([1, 1]),
+        }
+        super(King, self).__init__(rewards)
 
 
 class Knight(Piece):
     pass
-
-
-r = np.zeros((8, 8))
-k = King(0, 0, r)
-
-print(k.state())
-print(k.reward())
